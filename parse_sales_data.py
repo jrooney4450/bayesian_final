4b23c12e1ba0f142c5785e2b319303e051dffe9e
Justin Rooneygit config --global user.name Justin <justin@dexai.com>
2019-12-12 10:50:44 -0500

    feat: parser handles Nones and excpetions

--- parse_sales_data.py
+++ parse_sales_data.py
@@ -6,6 +6,8 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 import folium
+import time
+from datetime import datetime,date,time
 
 def importAndCleanData(threshold):
     print('Starting sales price parser')
@@ -17,15 +19,7 @@ def importAndCleanData(threshold):
     print('Reading data')
     df = pd.read_csv(data_path)
 
-    # # Here are a bunch of useful pd methods
-    # print(df.loc[3:4]) # Return numbered row, can be indexed
-    # print(df['SALE PRICE']) # Returns column
-    # print(df['BOROUGH'].values) # Return values of column, can index from here or manipulate
-    # print(df.info()) # Get meta-data about columns
-    # print(df.columns) # Get list of columns
-    # print(df.head()) # Preview of first 5 rows
-    # print(df.tail(2)) # Preview of last indices
-    # print(df.describe().loc['mean'])
+    # Here are all of the column strings for isolating data
 
     # All column data
     # BOROUGH                           84548 non-null int64
@@ -74,14 +68,18 @@ def importAndCleanData(threshold):
     return df
 
 def locationFinderToCSV(df):
+    print('Starting location to lat/long parser')
+
     # Gather relevant columns for robust geopy search
     sale_prices = df['SALE PRICE'].values
     zip_codes = df['ZIP CODE'].values
     addresses = df['ADDRESS'].values
     buroughs = df['BOROUGH'].values
     neighborhoods = df['NEIGHBORHOOD'].values
+    gross_sq_feet = df['GROSS SQUARE FEET']
+    land_sq_feet = df['LAND SQUARE FEET']
 
-    # onvert numeric arrays to str types
+    # Convert numeric arrays to str types
     zip_codes = zip_codes.astype('str')
     buroughs = buroughs.astype('str')
 
@@ -97,52 +95,83 @@ def locationFinderToCSV(df):
         if buroughs[idx] == '5':
             buroughs[idx] = 'STATEN ISLAND'
 
-    addresses = addresses + ', ' + neighborhoods + ', ' + buroughs + ', NEW YORK, NY, ' + zip_codes 
-    data = np.vstack((sale_prices, addresses)).T
+    addresses = addresses + ', ' + neighborhoods + ', ' + buroughs + ', NEW YORK, NY, ' + zip_codes
+    locations = np.zeros((len(addresses),))
+    latitudes = np.zeros((len(addresses),))
+    longitudes = np.zeros((len(addresses),))
+
+    data = np.vstack((sale_prices, addresses, latitudes, longitudes, neighborhoods, buroughs, zip_codes, gross_sq_feet, land_sq_feet)).T
 
     # Shuffle and take random data draw of size N if desired
     np.random.shuffle(data)
-    N = 3
+    N = 10
     data = data[0:N, :]
-
-    df = pd.DataFrame({'sale price':data[:, 0],
-                        'address':data[:, 1]})
-
-    locator = Nominatim(user_agent="myGeocoder")
 
     # Followed this tutorial: https://towardsdatascience.com/geocode-with-python-161ec1e62b89
     # Conveneint function to delay between geocoding calls
-    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+    locator = Nominatim(user_agent="myGeocoder")
 
-    # Create location column
-    df['location'] = df['address'].apply(geocode)
+    # Parse out None's and exceptions
+    pop_index = []
+    rejected_addresses = []
+    for i in range(N):
+        print('Count is at {} out of {}'.format(i+1, N))
+        
+        while True:
+            try:
+                location = locator.geocode(data[i, 1])
+                break
+            except:
+                print('geocoder service not working')
+                # pop_index.append(i)
+                # rejected_addresses.append(data[i, 1])
+        
+        if location is None:
+            print('None found')
+            data[i, 2] = None # lat
+            data[i, 3] = None # long
+            pop_index.append(i)
+            rejected_addresses.append(data[i, 1])
+        else:
+            data[i, 2] = location.latitude
+            data[i, 3] = location.longitude
+        # time.sleep(1)
 
-    locations = df['location'].values
+    # Arrange into new DataFrame
+    df2 = pd.DataFrame({'sale price':data[:, 0],
+                        'address':data[:, 1],
+                        'latitude':data[:, 2],
+                        'longitude':data[:, 3],
+                        'neighborhood':data[:, 4],
+                        'burough':data[:, 5],
+                        'zip code':data[:, 6],
+                        'gross sq feet':data[:, 7],
+                        'land sq feet':data[:, 8]})
 
-    # Find and remove None location values
-    print('size before {}'.format(df.shape))
-    drop_index = []
-    for i in range(len(locations)):
-        if locations[i] is None:
-            drop_index.append(i)
-    df = df.drop(drop_index, axis=0)
-    print('size after  {}'.format(df.shape))
+    # Find and remove None location values from stored pop_indices
+    print('DataFrame size before {}'.format(df2.shape))
+    df2 = df2.drop(pop_index, axis=0)
+    print('DataFrame size after  {}'.format(df2.shape))    
 
-    # Create longitude, laatitude and altitude from location column (returns tuple)
-    df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
-    # # 4 - split point column into latitude, longitude and altitude columns
-    df[['latitude', 'longitude', 'altitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
+    print(df2.head())
 
-    print(df.head())
+    today = date.today()
+    today_string = str(today.year) + str(today.month) + str(today.day)
 
-    # Save to new csv file
-    # # # # df.to_csv('data/nyc_property_loc.csv')
+    # Save data to new csv file with csv title including number of samples and time
+    df2.to_csv('data/nyc_sales_loc_{}_{}.csv'.format(df2.shape[0], today_string))
+
+    # Save rejected addresses with csv title including number of samples and time
+    df_rejected = pd.DataFrame({'addresses':rejected_addresses})
+    df_rejected.to_csv('data/nyc_sales_rejected_{}_{}.csv'.format(df_rejected.shape[0], today_string))
+
+    print('Done!')
 
 def main():
-    # Retuns original dataFrame with clean sale price data
-    df = importAndCleanData(5000)
+    # Retuns original DataFrame with clean sale price data
+    df = importAndCleanData(10000)
 
-    # Returns new dataFrame with clean location data
+    # Saves out new DataFrame with clean latitude and longitude data
     locationFinderToCSV(df)
 
 if __name__ == "__main__":
